b5812940ee9955c5eb11f45379cc9d7ce6de9f5d
Kyubeom Kim <kyubeom_kim@sk.com>
2019-09-27 08:48:34 +0900

    txtBuilder.js 개발 - 임시파일 처리를 위해 tmp 의존성 설치 - 트레이닝 후 delete() 메소드를 통해 파일 삭제처리

--- modules/txtBuilder.js
+++ modules/txtBuilder.js
@@ -1,1 +1,29 @@
-module.exports = txtMaker;
+// 사용자가 Export XML 기능을 실행했을 때, request의 xml 변환용 데이터를 사용한다.
+const tmp = require('tmp');
+const fs = require('fs');
+const os = require('os');
+
+let tmpFile;
+
+let txtBuilder = {
+  build: (subArray) => {
+    tmpFile = tmp.fileSync({keep: true});
+    console.debug(tmpFile.name);
+    let txt = "";
+
+    subArray.forEach((element) => {
+      txt += element.text + os.EOL;
+    });
+
+    fs.writeFile(tmpFile.name, txt, (err) => {
+      if (err) {
+        console.debug('error: ', err)
+      }
+    });
+  },
+  delete: () => {
+    tmpFile.removeCallback();
+  }
+}
+
+module.exports = txtBuilder;
